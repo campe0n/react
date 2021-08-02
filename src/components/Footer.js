6f63902a55ac49c69ffaa99a6aaa82d09d0c717b
Melvin <melvinrf@outlook.com>
2021-08-01 20:30:10 -0700

    styled page layout

---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+export default function Footer() {
+    return (
+        <div>
+            <ul>
+                <li>Github</li>
+                <li>LinkedIn</li>
+            </ul>
+        </div>
+    )
+}
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,29 @@
+const styles = {
+    header: {
+        display: 'flex',
+        justifyContent: 'space-between'
+    },
+    nav: {
+        display: 'flex',
+        justifyContent: 'center',
+        alignItems: 'center',
+        listStyleType: 'none',
+    },
+    li: {
+        margin: '1rem'
+    }
+
+}
+
 export default function Header() {
     return (
-        <header>
+        <header style={styles.header}>
             <h1>Melvin</h1>
-            <nav>
-                <li><a href='/about'>About</a></li>
-                <li><a href='/project'>Projects</a></li>
-                <li><a href='/resume'>Resume</a></li>
-                <li><a href='/contact'>Contact</a></li>
+            <nav style={styles.nav}>
+                <li style={styles.li}><a href='/about'>About</a></li>
+                <li style={styles.li}><a href='/project'>Projects</a></li>
+                <li style={styles.li}><a href='/resume'>Resume</a></li>
+                <li style={styles.li}><a href='/contact'>Contact</a></li>
             </nav>
         </header>
     )
