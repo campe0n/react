85f94acd34afb9004f36b858772de9fe7849b2db
Melvin <melvinrf@outlook.com>
2021-08-03 10:07:59 -0700

    added button components and new color scheme

---
+++ src/components/Nav.js
@@ -0,0 +1,18 @@
+export default function Nav() {
+  return (
+    <nav>
+      <li>
+        <a href="/about">About</a>
+      </li>
+      <li>
+        <a href="/project">Projects</a>
+      </li>
+      <li>
+        <a href="/resume">Resume</a>
+      </li>
+      <li>
+        <a href="/contact">Contact</a>
+      </li>
+    </nav>
+  );
+}
