0e216ff6e0a10f9cd7c11a8a7114fb6a2b36c6c6
melvin <melvinrf@outlook.com>
2021-09-11 17:33:38 -0700

    refactored navbar

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,18 +1,20 @@
 export default function Nav() {
   return (
     <nav>
-      <li>
-        <a href="/about">About</a>
-      </li>
-      <li>
-        <a href="/project">Projects</a>
-      </li>
-      <li>
-        <a href="/resume">Resume</a>
-      </li>
-      <li>
-        <a href="/contact">Contact</a>
-      </li>
+      <ul>
+        <li>
+          <a href="/about">About</a>
+        </li>
+        <li>
+          <a href="/project">Projects</a>
+        </li>
+        <li>
+          <a href="/resume">Resume</a>
+        </li>
+        <li>
+          <a href="/contact">Contact</a>
+        </li>
+      </ul>
     </nav>
   );
 }
