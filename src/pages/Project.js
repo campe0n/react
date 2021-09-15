154f22537c7738b0b847e275f6d0d8ff68921252
melvin <melvinrf@outlook.com>
2021-09-15 15:31:56 -0700

    mobile responsive

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -8,39 +8,55 @@ export default function Project() {
   return (
     <div class="deck">
       <h2>My projects</h2>
-      <p>Click on the images to be taken to the github page</p>
-      <div class="overlay">
-        <div class="imgOverlay">
-          <a
-            href="https://github.com/gannacon/Project_1"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={holidaySearch} alt="Holiday Serach Project" />
-          </a>
+      <div class="imgContainer">
+        <p>Click on the images to be taken to the github page</p>
+        <div class="overlay">
+          <div class="imgOverlay">
+            <a
+              href="https://github.com/gannacon/Project_1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={holidaySearch} alt="Holiday Serach Project" />
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="overlay">
-        <div class="imgOverlay">
-          <a
-            href="https://github.com/brownnicholasj/the-event-planning"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={eventPlanning} alt="Event Planning Project" />
-          </a>
+        <p>
+          Holiday Search was the first project I worked on for my bootcamp. The
+          objective was to create an application that utilizes an api.
+        </p>
+        <div class="overlay">
+          <div class="imgOverlay">
+            <a
+              href="https://github.com/brownnicholasj/the-event-planning"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={eventPlanning} alt="Event Planning Project" />
+            </a>
+          </div>
         </div>
-      </div>
-      <div class="overlay">
-        <div class="imgOverlay">
-          <a
-            href="https://github.com/campe0n/dplanner_mf"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <img src={workdayscheduler} alt="Worday Scheduler Project" />
-          </a>
+        <p>
+          The Event Planning Site is an application that allows users to store
+          information for planning an event, but also stores information about
+          guests should the host need them later. I worked the front end for
+          this project.
+        </p>
+        <div class="overlay">
+          <div class="imgOverlay">
+            <a
+              href="https://github.com/campe0n/dplanner_mf"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <img src={workdayscheduler} alt="Worday Scheduler Project" />
+            </a>
+          </div>
         </div>
+        <p>
+          Work Day Scheduler allows users to manage their day by keeping track
+          of events throught the day.
+        </p>
       </div>
     </div>
   );
